d91e1b0580b083bc789a4e641e32e48824d78f11
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-10-19 15:30:56 +0600

    update

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="container d-flex bg-secondary text-warning rounded my-5">
-            <div>Note: All information on KidsHealth® is for educational purposes only. For specific medical advice, diagnoses, and treatment, consult your doctor.
+            <div>Note: All information on Drug-Store® is for educational purposes only. For specific medical advice, diagnoses, and treatment, consult your doctor.
 
                 © 2021 Drug-Store. All rights reserved.
 
